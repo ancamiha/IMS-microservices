7b3408d236243facdd4ac907e52aeebc237ea698
Anca Enache <anca_mihaela.enache@stud.acs.upb.com>
2024-01-15 00:18:33 +0200

    Cleanup

--- CartManager/src/main/java/com/ims/CartManager/CartService.java
+++ CartManager/src/main/java/com/ims/CartManager/CartService.java
@@ -17,22 +17,22 @@ public class CartService {
     private final CartRepository cartRepository;
 
     public List<Sweets> getCart(Long id) {
-        List<Long> sweetsIds = this.cartRepository.getByUserId(id).getProducts();
-        return this.sweetsRepository.findAllById(sweetsIds);
+        List<Long> sweetsIds = cartRepository.getByUserId(id).getProducts();
+        return sweetsRepository.findAllById(sweetsIds);
     }
 
     public ResponseEntity<String> addProdToCart(ProdOfCart prodOfCart) {
-        if (this.cartRepository.existsByUserId(prodOfCart.getUserId())) {
-            Cart cart = this.cartRepository.getByUserId(prodOfCart.getUserId());
+        if (cartRepository.existsByUserId(prodOfCart.getUserId())) {
+            Cart cart = cartRepository.getByUserId(prodOfCart.getUserId());
             if (!cart.addProductId(prodOfCart.getProduct()))
                 return ResponseEntity.ok("Already exists");
             else {
-                this.cartRepository.save(cart);
+                cartRepository.save(cart);
                 return ResponseEntity.ok("Success");
             }
         } else {
             List<Long> products = List.of(prodOfCart.getProduct());
-            this.cartRepository.save(new Cart(prodOfCart.getUserId(), products));
+            cartRepository.save(new Cart(prodOfCart.getUserId(), products));
             return ResponseEntity.ok("Cart created and product added");
         }
     }
@@ -40,9 +40,7 @@ public class CartService {
     public ResponseEntity<String> deleteProdFromCart(ProdOfCart prodOfCart) {
         if (cartRepository.existsByUserId(prodOfCart.getUserId())) {
             Cart cart = cartRepository.getByUserId(prodOfCart.getUserId());
-            System.out.println(cart);
             cart.getProducts().remove(prodOfCart.getProduct());
-            System.out.println(cart);
             cartRepository.save(cart);
             return ResponseEntity.ok("Product deleted");
         } else {
